8ea028eb96998b6bc6ccdd5d1665db0b46ccece6
John Nadeau <johnnnadeau@gmail.next-commit>
2025-02-21 14:06:19 -0500

    add treesitter

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "yaml",
+        "ruby",
+      },
+    },
+  },
+}
